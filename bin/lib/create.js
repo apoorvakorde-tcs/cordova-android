f16848f19fbd1c3c3b762ebb998bda364179084d
Apoorva Korde <1345945@India.TCS.com>
2017-02-15 16:22:08 +0530

    Copy gradle wrapper files during create

--- bin/lib/create.js
+++ bin/lib/create.js
@@ -251,10 +251,15 @@ exports.create = function(project_path, config, options, events) {
         setShellFatal(true, function() {
             var project_template_dir = options.customTemplate || path.join(ROOT, 'bin', 'templates', 'project');
             // copy project template
-            shell.cp('-r', path.join(project_template_dir, 'assets'), project_path);
+            shell.cp('-r', path.join(, 'assets'), project_path);
             shell.cp('-r', path.join(project_template_dir, 'res'), project_path);
             shell.cp(path.join(project_template_dir, 'gitignore'), path.join(project_path, '.gitignore'));
 			shell.cp('-r', path.join(project_template_dir, 'gradle'), project_path);
+			if (process.platform == 'win32') {
+				shell.cp(path.join(project_template_dir, 'gradlew.bat'), project_path);
+			} else {
+				shell.cp(path.join(project_template_dir, 'gradlew'), project_path);
+			}
 
             // Manually create directories that would be empty within the template (since git doesn't track directories).
             shell.mkdir(path.join(project_path, 'libs'));
